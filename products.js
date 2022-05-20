1e6011e520291f9090cb558f6f2a7ae573b3d8a8
Arlene Santos <webdev.arlene@gmail.com>
2022-05-20 11:35:52 +0100

    v1

--- products.js
+++ products.js
@@ -1,40 +1,37 @@
 let products = [
     {
-        name: "nome do produto1",
+        name: "produto1",
         price: "1,00",
     },
     {
-        name: "nome do produto2",
+        name: "produto2",
         price: "2,00",
     },
     {
-        name: "nome do produto3",
+        name: "produto3",
         price: "3,00",
     },
     {
-        name: "nome do produto1",
+        name: "produto1",
         price: "4,00",
     },
     {
-        name: "nome do produto2",
+        name: "produto2",
         price: "5,00",
     },
     {
-        name: "nome do produto3",
+        name: "produto3",
         price: "6,00",
     },
     {
-        name: "nome do produto1",
+        name: "produto1",
         price: "7,00",
     },
     {
-        name: "nome do produto2",
+        name: "produto2",
         price: "8,00",
     },
-    {
-        name: "nome do produto2",
-        price: "9,00",
-    },
+
 
 
 
